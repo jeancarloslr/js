a336f1e2c327efa9590859caa9a91806e2aabb99
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-05-25 22:21:09 -0300

    exercicio

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,19 +1,19 @@
 
 function contar(){
 
-    var inicio = document.getElementById('inicio').value
-    var fim = document.getElementById('fim').value
-    var passo = document.getElementById('passo').value
+    var cont = Number(document.getElementById('inicio').value)
+    var fim = Number(document.getElementById('fim').value)
+    var passo = Number(document.getElementById('passo').value)
     var res = document.getElementById('res')
 
-    var cont = 0;
-    while(inicio <= fim){
 
-        var linha = document.createElement('p')
-        linha.textContent = `${cont}`
-        res.appendChild(linha)
+    while(cont <= fim){
 
-        cont = cont + passo
+        var linha = document.createElement('p');
+        linha.textContent = `${cont}`;
+        res.appendChild(linha);
+        cont += passo;
+        
     }
 
 }
