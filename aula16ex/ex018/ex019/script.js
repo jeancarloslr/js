360723549e36017bebeb9fbf30a9a4e000fe49c1
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-05-27 15:24:43 -0300

    try tabuada

--- aula16ex/ex018/ex019/script.js
+++ aula16ex/ex018/ex019/script.js
@@ -1,13 +1,4 @@
 
-function gerar(){
-
-    var number = Number(window.document.querySelector('#number').value)
-
-    for(cont = 1; cont <= 10; cont++){
-
-        console.log(number * cont)
-
-    }
-
-
+function contar(){
+    console.log('its working')
 }
