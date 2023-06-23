11987a36dd26ccf3abb0121f36e20375e640dafa
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-06-23 17:54:55 -0300

    analyszer number

--- desafioAnNum/script.js
+++ desafioAnNum/script.js
@@ -1,27 +1,33 @@
 
 function adicionar() {
 
-    var impt = window.document.getElementById("impt");
-    var select = window.document.getElementById("numbers");
+  var impt = window.document.getElementById("impt");
+  var select = window.document.getElementById("numbers");
+
+  if (impt.value.length == 0 || impt.value > 100) {
+
+    alert("Insira um número de 0 a 100");
+    impt.value = "";
+
+  } else {
+
+    var number = Number(impt.value);
+
+    let item = document.createElement('option')
+    item.text = `Número ${number} selecionado`
+    select.appendChild(item);
+
+    let options = [];
+    options.push(item.text);
     
+    console.log(options.sort());
+
+    impt.value = "";
     
-      if (impt.value.length == 0) {
-
-       alert("Insira um número de 0 a 100");   
-
-      }else{
-
-        var number = Number(impt.value);
-       /* tab.innerHTML = "";*/
-
-        let item = document.createElement('option')
-        item.text = `Número ${number} selecionado`
-        select.appendChild(item)
-        
-        let options = [];
-        options = item.text;
-        console.log(options)
-        
-    }
+  } 
 }
+
+
+   
     
+/**/
