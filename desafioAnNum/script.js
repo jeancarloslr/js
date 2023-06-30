7c6239e96c34f831dda4176e27843882baf5f7d1
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-06-30 19:46:05 -0300

    Começo

--- desafioAnNum/script.js
+++ desafioAnNum/script.js
@@ -1,39 +1,15 @@
+let num = window.document.getElementById('impt');
+let list = window.document.getElementById('lista');
+let result = window.document.getElementById('res');
+let valores = []
 
-function adicionar() {
+function adicionar(){
 
-  var impt = window.document.getElementById("impt");
-  var select = window.document.getElementById("numbers");
-
-  if (impt.value.length == 0 || impt.value > 100) {
-
-    alert("Insira um número de 0 a 100");
-    impt.value = "";
-
-  } else {
-
-    var number = Number(impt.value);
-
-    if(number == item.value){
-      alert('Digite um valor diferente');
-    }
-
-    let item = document.createElement('option')
-    item.text = `${number}`
-    select.appendChild(item);
-
-    let options = [];
-    options.push(number);
-    
-    console.log(options.sort());
-
-    impt.value = "";
-    
-  } 
-}
-
-function Finalizar(){
-
+ if(isNumero(num.value) && !inLista(num.value)){
+ }
 
 }
 
-/**/
+/*var item = document.createElement('option')
+item.text = `${number}`
+select.appendChild(item);*/
