b68979e2ca5e1c0982cf1b8ddf49cbc7610487a3
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-07-01 16:43:53 -0300

    função isNumero(n)

--- desafioAnNum/script.js
+++ desafioAnNum/script.js
@@ -3,9 +3,32 @@ let list = window.document.getElementById('lista');
 let result = window.document.getElementById('res');
 let valores = []
 
+function isNumero(n){
+
+    if(Number(n) >= 1 && Number(n) <= 100){
+
+        return true;
+      
+  }else{
+    
+    return false;
+
+  }
+}
+
+function inLista(n, L){
+
+}
+
+
 function adicionar(){
 
- if(isNumero(num.value) && !inLista(num.value)){
+ if(isNumero(num.value) && !inLista(num.value, valores)){
+
+
+
+ }else{
+    window.alert('Valor inválido ou já encontrado na lista')
  }
 
 }
