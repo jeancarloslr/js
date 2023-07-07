e891afbf0c6c126270263063282e966739f69ed1
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-07-07 15:18:46 -0300

    nothing

--- desafioAnNum/script.js
+++ desafioAnNum/script.js
@@ -1,44 +1,31 @@
-let num = window.document.getElementById('impt');
+let num = document.querySelector('#impt')
 let list = window.document.getElementById('lista');
 let result = window.document.getElementById('res');
 let valores = []
 
 function isNumero(n){
-
     if(Number(n) >= 1 && Number(n) <= 100){
-
         return true;
-      
   }else{
-    
     return false;
-
   }
 }
 
 function inLista(n, l){
-
   if(l.indexOf(Number(n)) != -1){
       return true;
   }else{
     return false;
   }
-
 }
 
 function adicionar(){
-
  if(isNumero(num.value) && !inLista(num.value, valores)) {
-
   console.log('tudo ok')
-
  }else{
-    window.alert('Valor inválido ou já ncontrado na lista')
+    window.alert('Valor inválido ou já ncontrado na lista.')  
  }
- 
-
 }
-
 
 /*var item = document.createElement('option')
 item.text = `${number}`
