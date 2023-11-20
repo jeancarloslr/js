cf65819101a50005115f6f1d4ec3929466fba4ad
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-11-20 19:44:56 -0300

    password validator done.

--- funções_callbacks/validaçãoSenha/script.js
+++ funções_callbacks/validaçãoSenha/script.js
@@ -1,34 +1,29 @@
 var inpt = window.document.getElementById('password');
 var btn = window.document.getElementById('btn-tester');
 var msg = window.document.getElementById('msg')
-
 btn.addEventListener('click', validarSenha);
-
 function validarSenha() {
     function autenticarSenha(sucessSenha, errorSenha) {
         var temMaiuscula = /[A-Z]/.test(inpt.value);
         var temMinuscula = /[a-z]/.test(inpt.value);
+         /*var temNumero = /\d/.test(inpt.value);*/
+        var numberEspecial = /^(?=.*\d)(?=.*[!@#$%^&*])/.test(inpt.value);
 
-
-        if (inpt.value.length >= 8 && temMaiuscula && temMinuscula) {
+        if (inpt.value.length >= 8 && temMaiuscula && temMinuscula && numberEspecial) {
             sucessSenha('Boa');
+            inpt.style = "border: 3px solid green;";
         } else {
-            errorSenha('Erro');
+            errorSenha('');
             inpt.style = "border: 1px solid red;";     
-            inpt.value = "";       
         }
     }
-
-
     var sucessSenha = function (mensagem) {
         msg.innerHTML = mensagem; 
         inpt.value = "";
     }
-
     var errorSenha = function (mensagemErro) {
-        console.log(mensagemErro);
+        msg.innerHTML = mensagemErro;
+        inpt.value = "";
     }
-
     autenticarSenha(sucessSenha, errorSenha);
 }
-
