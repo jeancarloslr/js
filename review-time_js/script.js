c51ae333e99b66bc0d027b0d9d8eec984c285f4a
Jean Carlos <jeanxh4@gmail.com>
2024-03-22 23:06:43 -0300

    review time js

--- review-time_js/script.js
+++ review-time_js/script.js
@@ -1,6 +1,21 @@
 const timeNow = document.querySelector('.timerNow');
 const timerContado = document.querySelector('.timerContado');
 
+let ContagemRegressiva = 60;
+
+const btnStop = document.querySelector('.btnStop');
+const btnGo = document.querySelector('.btnGo');
+
+btnStop.addEventListener('click', () =>{
+    if(intervaloId){
+        clearInterval(intervaloId)
+        intervaloId = null;
+    }
+})
+
+btnGo.addEventListener("click", () =>{
+    intervaloId = setInterval(contar, 1000);
+});
 const timersy = new Date();
 const hora = timersy.getHours();
 const minutos = timersy.getMinutes();
@@ -11,6 +26,10 @@ const textoFormatado = `${hora}:${minutos}`;
 
 timeNow.innerHTML = `${textoFormatado}`;
 
+function contar(){
+    timerContado.innerHTML = `${ContagemRegressiva}`;
+    ContagemRegressiva -= 1;
+}
 
-
+let intervaloId = setInterval(contar, 1000);
 
